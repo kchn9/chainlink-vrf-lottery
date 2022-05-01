db9d435fde3a30d98ae0f4b10d3284894a54dc90
kchn9 <mat.kuchnia@gmail.com>
2022-05-01 18:28:55 +0200

    Add RNG request

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -3,23 +3,35 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 
-contract Lottery is Ownable {
+contract Lottery is Ownable, VRFConsumerBaseV2 {
     using Counters for Counters.Counter;
 
     Counters.Counter participantCounter;
     mapping ( uint => address ) participants;
+
     uint256 private _balance;
 
-    uint256 immutable public reward;
-    uint256 immutable minParticipate;
-    uint256 immutable endsAt;
+    VRFCoordinatorV2Interface immutable private _coordinator;
+    uint64 immutable private _VRFSubId;
 
-    constructor(uint256 _minParticipate, uint64 endsIn) payable {
+    // valid for Rinkeby
+    address private _coordinatorAddress = 0x6168499c0cFfCaCD319c818142124B7A15E857ab;
+    bytes32 private _keyHash = 0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc;
+
+    uint256 immutable public reward;
+    uint256 immutable public minParticipate;
+    uint256 immutable private _endsAt;
+
+    constructor(uint256 _minParticipate, uint64 _endsIn, uint64 _VRFSubscriptionId) VRFConsumerBaseV2(_coordinatorAddress) payable {
         require(msg.value > 0, "Lottery: Reward cannot be 0.");
         reward = msg.value;
         minParticipate = _minParticipate;
-        endsAt = block.timestamp + endsIn;
+        _coordinator = VRFCoordinatorV2Interface(_coordinatorAddress);
+        _VRFSubId = _VRFSubscriptionId;
+        _endsAt = block.timestamp + _endsIn;
     }
 
     function participate() public payable {
@@ -32,7 +44,14 @@ contract Lottery is Ownable {
 
     function drawWinner() public {
         require(getTimeLeft() == 0, "Lottery: Lottery is not closed yet.");
-        // todo
+        _requestRandomWords();
+    }
+
+    function _requestRandomWords() internal {
+        _coordinator.requestRandomWords(_keyHash, _VRFSubId, 3, 100000, 1); // reqConfs, gasCallLimit, reqWords
+    }
+
+    function fulfillRandomWords(uint256 /* requestId */, uint256[] memory _randWords) internal override {
     }
 
     function withdraw() public onlyOwner {
@@ -42,8 +61,8 @@ contract Lottery is Ownable {
     }
 
     function getTimeLeft() public view returns(uint) {
-        if (block.timestamp >= endsAt) return 0;
-        return endsAt - block.timestamp;
+        if (block.timestamp >= _endsAt) return 0;
+        return _endsAt - block.timestamp;
     }
 
     function getBalance() public view returns(uint) {
